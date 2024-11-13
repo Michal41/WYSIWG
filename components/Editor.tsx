1514b85fd1e5c2d6033a30a05441994140e1d730
Michal Kanarek <michal.kanarek@gmail.com>
2024-11-13 19:39:06 +0100

    Add editor package

---
+++ components/Editor.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import React from 'react'
+import { useEditor, EditorContent } from '@tiptap/react'
+import StarterKit from '@tiptap/starter-kit'
+
+const Editor = () => {
+
+  const editor = useEditor({
+    extensions: [StarterKit],
+    content: 'xxxx',
+  })
+
+  return (
+    <div className='bg-red bg-[yellow]'>
+      <EditorContent editor={editor} />
+    </div>
+  )
+}
+
+export default Editor
