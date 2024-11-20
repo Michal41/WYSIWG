daf661a4b0b7fb1f362241c6d3c3d3abc48be465
Michal Kanarek <michal.kanarek@gmail.com>
2024-11-20 16:34:45 +0100

    Mark contract versions to compare

--- src/components/ContractVersionTile.tsx
+++ src/components/ContractVersionTile.tsx
@@ -3,13 +3,21 @@ import { IContractDocument } from "@/models/ContractDocument";
 
 type Props = {
   contractDocument: IContractDocument;
+  active: boolean;
+  onClick: () => void;
 };
 
-const ContractVersionTile = ({ contractDocument }: Props) => {
+const ContractVersionTile = ({ contractDocument, active, onClick }: Props) => {
   return (
     <div
+      style={{
+        backgroundColor: active ? "#1a1919" : "white",
+        color: active ? "white" : "#1a1919",
+        fontWeight: active ? "bold" : "normal",
+      }}
       className="flex flex-col justify-center items-center border border-gray-300 rounded-md p-4 cursor-pointer"
       key={contractDocument.createdAt?.toString()}
+      onClick={onClick}
     >
       {new Date(contractDocument.createdAt ?? "").toLocaleDateString()}
     </div>
--- src/components/ContractVersions.tsx
+++ src/components/ContractVersions.tsx
@@ -3,9 +3,13 @@ import { index } from "@/services/contractDocuments";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import ContractVersionTile from "./ContractVersionTile";
+import Button from "./Button";
 
 const ContractVersions = () => {
   const { contractId } = useParams();
+  const [activeContractDocumentId, setActiveContractDocumentId] = useState<
+    string[]
+  >([]);
   const [contractDocuments, setContractDocuments] = useState<
     IContractDocument[]
   >([]);
@@ -18,14 +22,33 @@ const ContractVersions = () => {
     };
     fetchContractDocuments();
   }, [contractId]);
+
+  const addContractToActive = (contractDocumentId: string) => {
+    if (activeContractDocumentId.includes(contractDocumentId)) {
+      setActiveContractDocumentId(
+        activeContractDocumentId.filter((id) => id !== contractDocumentId),
+      );
+    } else {
+      if (activeContractDocumentId.length < 2) {
+        setActiveContractDocumentId([
+          ...activeContractDocumentId,
+          contractDocumentId,
+        ]);
+      }
+    }
+  };
+
   return (
     <div className="flex flex-col justify-center items-center gap-4 mt-4">
       {contractDocuments.map((contractDocument) => (
         <ContractVersionTile
           contractDocument={contractDocument}
           key={contractDocument._id}
+          active={activeContractDocumentId.includes(contractDocument._id)}
+          onClick={() => addContractToActive(contractDocument._id)}
         />
       ))}
+      <Button label="Compare" onClick={() => setActiveContractDocumentId([])} />
     </div>
   );
 };
